20090767fa0aea06350f9c38a91acd073a2f81b8
Sheng <ericx1023@gmail.com>
2023-11-20 12:47:35 -0800

    add title

--- src/components/templates/header/header.tsx
+++ src/components/templates/header/header.tsx
@@ -1,13 +1,14 @@
 import { useTranslation } from 'next-i18next';
 import Link from 'next/link';
 import Image from 'next/image';
-import BlogLogo from '@icons/icon2.jpeg';
+import BlogLogo from '@icons/icon2.png';
 
 import { LanguageSelector } from '@src/components/features/language-selector';
 import { Container } from '@src/components/shared/container';
 
 export const Header = () => {
   const { t } = useTranslation();
+  console.log(t('common.homepage'));
 
   return (
     <header className="py-5">
@@ -16,6 +17,7 @@ export const Header = () => {
           <Link href="/" title={t('common.homepage')}>
             <Image src={BlogLogo} alt="logo" width={68} height={68} />
           </Link>
+          <p>{t('迷幻 矽谷')}</p>
           <LanguageSelector />
         </Container>
       </nav>
