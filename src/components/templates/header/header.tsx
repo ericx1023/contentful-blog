7f0ebbb64d41e1ed1849e100e7efbe7f859eb47f
Sheng <ericx1023@gmail.com>
2025-07-29 22:00:40 -0700

    Chore(assets): remove old icons and add new TAPSC logo; update favicon images

--- src/components/templates/header/header.tsx
+++ src/components/templates/header/header.tsx
@@ -1,13 +1,15 @@
 import { useTranslation } from 'next-i18next';
+import { useRouter } from 'next/router';
 import Link from 'next/link';
 import Image from 'next/image';
-import BlogLogo from '@icons/logo.png';
+import BlogLogo from '@icons/TAPSC.png';
 
 import { LanguageSelector } from '@src/components/features/language-selector';
 import { Container } from '@src/components/shared/container';
 
 export const Header = () => {
   const { t } = useTranslation();
+  const router = useRouter();
 
   return (
     <header className="border-b border-blue-medium bg-gradient-to-r from-blue-dark to-accent-slate py-4 text-white shadow-medium">
@@ -25,7 +27,9 @@ export const Header = () => {
           <div className="flex items-center space-x-8">
             <Link
               href="/"
-              className="text-base font-medium text-white transition-colors duration-200 hover:text-gray-lightest"
+              className={`text-2xl font-medium text-white transition-colors duration-200 hover:text-gray-lightest ${
+                router.locale === 'zh-Hant-TW' ? 'tracking-widest' : ''
+              }`}
             >
               {t('common.homepage')}
             </Link>
